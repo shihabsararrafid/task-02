0184013002a90368b7a63f0c592a74ef77fbbc55
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-06-22 06:59:05 +0600

    get new list from text area

--- src/Components/HomeMain/ToDo.js
+++ src/Components/HomeMain/ToDo.js
@@ -4,19 +4,35 @@ import uncheck from "./../../Images/Icon_Vector_Uncheck.png";
 import check from "./../../Images/Icon_Vector_Check.png";
 const ToDo = () => {
   const [select, setSelect] = useState(false);
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
+  console.log(todo);
+  const addToList = () => {
+    const newItem = todo;
+    const newList = [...list, newItem];
+    setList(newList);
+    setTodo("");
+    const element = document.getElementById("textarea");
+    element.value = "";
+  };
+  console.log(list);
   return (
     <div className="w-full h-[460px] mt-[37px] bg-white p-4">
       <h1 className="font-semibold text-base ">To Do List </h1>
       <div className="text-area flex">
         <textarea
           name=""
-          id=""
+          id="textarea"
           cols="65"
           rows="4"
+          onChange={(e) => setTodo(e.target.value)}
           className="border-[2px] focus:border-[#FF7978] outline-0 border-[#EDEBEB]"
         ></textarea>
         {/* bg-gradient-to-r from-[#FF7594] to-[#FF7C65] */}
-        <button className="flex h-[40px] ml-3  text-white items-center bg-gradient-to-r from-[#FF7594] to-[#FF7C65] py-1 px-3 rounded-md ">
+        <button
+          onClick={addToList}
+          className="flex h-[40px] ml-3  text-white items-center bg-gradient-to-r from-[#FF7594] to-[#FF7C65] py-1 px-3 rounded-md "
+        >
           <img src={small} className="" alt="" />
           <span> Add</span>
         </button>
