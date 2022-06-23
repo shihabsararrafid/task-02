83acc74aab59a421f4c5e6178ba2dbc9315ae611
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-06-23 16:55:23 +0600

    all design finish

--- src/Components/CoronaUpdate/CoronaUpdate.js
+++ src/Components/CoronaUpdate/CoronaUpdate.js
@@ -16,7 +16,7 @@ const CoronaUpdate = () => {
   var today = new Date().toLocaleTimeString();
   console.log(today);
   return (
-    <div className="corona-update p-6 mr-5 rounded-lg bg-white">
+    <div className="corona-update px-6 py-10 mr-5 rounded-lg bg-white">
       <div className="flex items-center font-semibold justify-between">
         <h1>Covid 19 Updates</h1>
         <div>
@@ -27,8 +27,14 @@ const CoronaUpdate = () => {
           </div>
         </div>
       </div>
-      <div className="mt-10">
+      <div className="mt-10 flex">
         <img className="w-[50%] h-[70%] object-cover" src={corona} alt="" />
+        <div className="w-[50%] text-white font-medium rounded-lg px-5 py-8 text-center bg-gradient-to-r from-[#FF7594] to-[#FF7C65]">
+          <p>Infection Number </p>
+          <p className="text-4xl  text-white ">2000</p>
+          <p>Infection Rate </p>
+          <p className="text-4xl text-white">11%</p>
+        </div>
       </div>
     </div>
   );
